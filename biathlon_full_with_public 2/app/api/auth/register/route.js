e433dc5da3fc7ad5ef6b9562c1bc61e08b4f4506
tkerhoas <tkerhoas@lequipe.fr>
2025-12-01 23:56:23 +0100

    Delete biathlon_full_with_public 2 directory

--- biathlon_full_with_public 2/app/api/auth/register/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function POST(req) {
-  const body = await req.json()
-  console.log("Nouveau compte :", body)
-  return NextResponse.json({ ok: true })
-}
-
--- biathlon_full_with_public 2/app/layout.js
+++
@@ -1,6 +0,0 @@
-import Header from '../components/Header'
-...
-<body>
-  <Header />
-  <main>{children}</main>
-</body>
